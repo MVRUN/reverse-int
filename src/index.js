7854653e445f1fb1bcb2dbc0b055273404146017
MVRUN <55558413+MVRUN@users.noreply.github.com>
2021-02-22 15:41:53 +0300

    feat
    Add a solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    if (n >= 0) {
+  n = n.toString().split("").reverse().join("");
+  return +n;
+    }
+    else {
+        n = Math.abs(n);
+        n = n.toString().split("").reverse().join("");
+        return +n;
+    }
 }
